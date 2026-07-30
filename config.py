86016bfa298f24f0dcdb1b92ee0b0b85e83cab22
tarakanus10k <vitalikromanuk908@gmail.com>
2026-07-30 22:54:13 +0300

    README change, new config dir, new utils dir, new logger.py file, new log_reader.py file

--- config.py
+++ config.py
@@ -14,15 +14,15 @@ class LOGCONFIG:
     name: str
     path: str
 
-def load_config(conf_dir: str = "conf.d") -> list[LOGCONFIG]:
-    log_configs = []
-    for file_path in glob.glob(os.path.join(conf_dir, "*.y*ml")):
-        with open(file_path, "r", encoding="utf-8") as file:
-            data = yaml.safe_load(file)
-            for log_def in data["logs"]:
-                log_configs.append(LOGCONFIG(
-                    name=log_def.get("name", os.path.basename(file_path)),
-                    path=log_def["path"]
-                ))
+    def load_config(conf_dir: str = "config/conf.d") -> list[LOGCONFIG]:
+        log_configs = []
+        for file_path in glob.glob(os.path.join(conf_dir, "*.y*ml")):
+            with open(file_path, "r", encoding="utf-8") as file:
+                data = yaml.safe_load(file)
+                for log_def in data["logs"]:
+                    log_configs.append(LOGCONFIG(
+                        name=log_def.get("name", os.path.basename(file_path)),
+                        path=log_def["path"]
+                    ))
 
-        return log_configs
+            return log_configs
